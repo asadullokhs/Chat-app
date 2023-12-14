3ca5b38c0ad428bac915b01f70393c86de9d1bd7
Asadullokh <uzbdevit@gmail.com>
2023-12-15 03:44:38 +0500

    added socket io

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const socketIo = require("socket.io");
 const mongoose = require("mongoose");
+const http = require("http");
 
 const path = require("path");
 dotenv.config();
@@ -15,6 +17,15 @@ const messageRouter = require("./src/router/messageRouter");
 
 const app = express();
 const PORT = process.env.PORT || 4002;
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    // origin: "netlify"
+    // methods: ["GET", "POST", "DELETE"]
+    origin: "http://localhost:3000",
+  },
+});
 
 // Static file
 app.use(express.static(path.join(__dirname, "src", "public")));
@@ -31,12 +42,38 @@ app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
 
+// websocket functions
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  socket.on("new-user-added", (newUserId) => {
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+    }
+
+    io.emit("get-users", activeUsers);
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+
+    io.emit("get-users", activeUsers);
+  });
+
+  socket.on("exit", (id) => {
+    activeUsers = activeUsers.filter((user) => user.userId !== id);
+
+    io.emit("get-users", activeUsers);
+  });
+});
+
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server responded at ${PORT} PORT...`);
     });
   })
